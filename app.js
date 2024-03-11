d9b8f00dff0978d0a1a6a37131d8ced0667b813e
bportugal01 <bruno2004sa@gmail.com>
2024-03-11 20:58:44 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars').engine
+
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', function (req, res) {
+    res.render('primeira_pagina')
+})
+
+app.listen(8081, function () {
+    console.log('A porra do servidor rodando no endereço http://localhost:8081')
+})
